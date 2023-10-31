d9e15ff42caba5ee45fc3143c76a7514b7e25733
yel106 <yel_@hanmail.net>
2023-10-31 15:21:34 +0900

    jsx의 {}에서 간단한 연산자 활용 &&, ||

--- src/App.js
+++ src/App.js
@@ -1,50 +1,26 @@
 function App() {
-  // jsx에서 태그 사용시 꼭 종료태그를 작성해야함
-  return (
-    <div>
-      <h1>hello react</h1>
-      <h2>HI yelin</h2>
-      <p>
-        Lorem ipsum dolor.
-        <br /> lorem ipsum dolor.
-      </p>
-      <MyComp />
-      <MyCompo2 />
-    </div>
-  );
-}
+  // 1~100
+  const number = Math.ceil(Math.random() * 100);
+  const isLarge = number > 50;
+  const isSmall = number <= 50;
 
-// built-in component는 소문자로 시작
-// component명은 꼭 대문자로 시작해야함
-function MyComp() {
-  // return에 jsx 코드 작성 가능
-  // 꼭 하나의 root tag가 존재해야 함
   return (
     <>
-      {/* 주석 사용법 */}
-      {/* fragment는 <></>  */}
-      <h1>HI COMPONENT</h1>
-      <h2>hello</h2>
+      <h1>난수: {number}</h1>
+      <div>{number > 50 ? "큰 수" : "작은 수"}</div>
+      <div>{number > 50 ? <BigImage /> : <SmallImage />}</div>
+      <div>{isLarge && <h1>큰 수이다</h1>}</div>
+      <div>{isLarge || <h1>작은 수이다.</h1>} </div>
     </>
   );
 }
 
-function MyCompo2() {
-  let name = "손흥민";
-  let age = 33;
+function BigImage() {
+  return <h1>큰 이미지</h1>;
+}
 
-  // {}: jsx에서 js 코드 작성하기 위한 기호
-  // {}를 쓰면 자바스크립트 코드로 인식함
-  return (
-    <>
-      <h1>hello react</h1>
-      <h1>hello {name}</h1>
-      <h1>{age} years</h1>
-      <h1>{age * 2} double years</h1>
-      <h1>{name + "선수"}</h1>
-      {/* 그래서 주석이 이렇게 생김 */}
-    </>
-  );
+function SmallImage() {
+  return <h3>작은 이미지</h3>;
 }
 
 export default App;
