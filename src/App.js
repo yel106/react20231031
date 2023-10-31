9bd0e45cb9cc38cf8fa95b51e49a0b686a2396c7
yel106 <yel_@hanmail.net>
2023-10-31 16:34:32 +0900

    텍스트 가운데 정렬 인라인 스타일로 적용하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-
 function App() {
   return (
-    <div>
+    <>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <MyComp />
+        <MyComp />
+        <MyComp />
+      </div>
+    </>
+  );
+}
 
+function MyComp() {
+  const name = "흥민";
+  return (
+    <div
+      style={{
+        backgroundColor: "yellow",
+        color: "black",
+        padding: "10px",
+        width: "25%",
+        textAlign: "center",
+      }}
+    >
+      {name}
     </div>
   );
 }
