19ea79a12e0dbdee18a5e38a099dd88a855f02c3
yel106 <yel_@hanmail.net>
2023-10-31 15:34:29 +0900

    jsx의 {}에서 간단한 연산자 활용 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
 function App() {
-  return (
-    <div>
+  const num1 = Math.ceil(Math.random() * 6);
+  const num2 = Math.ceil(Math.random() * 6);
+  let message;
 
-    </div>
+  if (num1 === num2) {
+    message = <h1>당첨</h1>;
+  } else {
+    message = <h1>다시 던져 보세요.</h1>;
+  }
+
+  return (
+    <>
+      <h1>첫 번째 숫자: {num1}</h1>
+      <h1>두 번째 숫자: {num2}</h1>
+      <h1>{num1 == num2 ? "당첨!!" : "다시 던져보세요!"}</h1>
+
+      <hr />
+
+      {message}
+    </>
   );
 }
 
