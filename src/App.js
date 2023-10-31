cc8970f6666754ba1ae56f7b6c03c1b91f1df913
yel106 <yel_@hanmail.net>
2023-10-31 15:48:54 +0900

    property의 값은 자바스크립트 코드가 될 수 있음

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 function App() {
-  // JSX가 JS 코드로 변환됨
-  // 따라서 JS 키워드를 사용할 수 없음
+  const name = "민지";
 
   return (
     <>
-      {/* class 속성은 className으로 작성 해야함 */}
-      <div className="header">lorem ipsum dolor.</div>.
-      <div className="error">lorem ipsum dolor.</div>.
-      {/* for 속성은 htmlFor로 작성*/}
-      <div>
-        <label htmlFor="nameInput">이름</label>
-        <input type="text" id="nameInput" />
-      </div>
-      <div>
-        <label htmlFor="ageInput"></label>
-        <input type="text" id="ageInput" />
-      </div>
+      <h1>이름은 {name}</h1>
+      <h1>이름은 {"강인"}</h1>
+      <h1>이름은 희찬</h1>
+
+      <div className={"error"}>Lorem ipsum dolor.</div>
+      <div className="error">Lorem ipsum dolor.</div>
     </>
   );
 }
