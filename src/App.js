75f63bf419bc9a399ae44ecd2014032c23580219
yel106 <yel_@hanmail.net>
2023-10-31 15:10:58 +0900

    {} 자바스크립트 코드

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+function MyComp() {
+  const address = "seoul";
+  const country = "korea";
+  const phone = "01012345678";
+  const age = 44;
+  const randomNumber = Math.random();
+  return (
+    <>
+      <h1> age는 {age}</h1>
+      <h1> address는 {address}</h1>
+      <h1> country는 {country}</h1>
+      <h1> 전화번호는 {phone}</h1>
+      <h1> 난수: {randomNumber}</h1>
+      <h1> 난수 1~10: {Math.ceil(randomNumber * 10)}</h1>
+    </>
+  );
+}
 
 function App() {
   return (
     <div>
-
+      <MyComp />
+      {/*
+      <h1> age는 44 </h1>
+      <h1> country는 korea </h1>
+      <h1> 전화번호는 01012345678 </h1>
+      */}
     </div>
   );
 }
