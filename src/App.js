e7a9a18f629c6a7d1e89d79b041d2f7e07bfac1d
yel106 <yel_@hanmail.net>
2023-10-31 12:01:42 +0900

    MyComponent1, 2만들어서 연습

--- src/App.js
+++ src/App.js
@@ -2,9 +2,43 @@
 function App() {
   return (
     <div>
-
+      <MyComponent1 />
+      {/*
+      <div>
+        <h1>Hello Component</h1>
+      </div>
+      */}
+      <MyComponent2 />
+      {/*
+        <p>lorem ipsum alkdjf alkdjf </p>
+        <ul>
+          <li>lorem</li>
+          <li>ipsum</li>
+          <li>dolor</li>
+        </ul>
+       */}
     </div>
   );
 }
 
+function MyComponent1() {
+  return (
+      <>
+      <h1>Hello Component</h1>
+      </>
+  )
+}
+
+function MyComponent2() {
+  return (
+      <>
+        <p>lorem ipsum alswjf alswjf</p>
+        <ul>
+          <li>lorem</li>
+          <li>ipsum</li>
+          <li>dolor</li>
+        </ul>
+      </>
+  )
+}
 export default App;
