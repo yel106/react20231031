66eac2cad15bcf3de9c878dc784758b5a8401af7
yel106 <yel_@hanmail.net>
2023-10-31 14:26:03 +0900

    종료태그 작성연습 완료, 컴포넌트 여러번 사용연습 완료, 이미지 올리기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-
 function App() {
   return (
     <div>
+      <MyComponent1 />
+      <MyComponent1 />
+      <MyComponent1 />
+      {/*
+          <img src="https://i.imgur.com/LkAyqxJ.jpeg" />
+          <img src="https://i.imgur.com/LkAyqxJ.jpeg" />
+          <img src="https://i.imgur.com/LkAyqxJ.jpeg" />
+        */}
+    </div>
+  );
+}
 
+function MyComponent1() {
+  return (
+    <div style={{ width: "100%" }}>
+      <img
+        style={{ width: "100%" }}
+        src="https://i.imgur.com/LkAyqxJ.jpeg"
+        alt="너굴"
+      />
+      ;
     </div>
   );
 }
