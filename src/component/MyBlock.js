0902673498f55828338686aab02d76ba21b7bdcb
yel106 <yel_@hanmail.net>
2023-11-01 16:21:01 +0900

    사용할 때 클래스명 중복이 발생할 수 있음 -> css module로 중복 방지

---
+++ src/component/MyBlock.js
@@ -0,0 +1,8 @@
+import style from "../style/yourStyle.module.css";
+export function MyBlock() {
+  return (
+    <div>
+      <h1 className={style.error}>Lorem ipsum dolor sit amet.</h1>
+    </div>
+  );
+}
--- src/component/MyText.js
+++ src/component/MyText.js
@@ -1,13 +1,13 @@
-import "../style/mystyle.css";
+import style from "../style/mystyle.module.css";
 
 export function MyText() {
   return (
     <>
       <div>
         {/* red 배경색, white 글자색 */}
-        {/* src/style/mystyle.css 파일 생성 */}
-        {/* MyText.js에서 mystyle.css import 하기 */}
-        <h1 className="error">Lorem ipsum dolor sit amet.</h1>
+        {/* src/style/mystyle.module.css 파일 생성 */}
+        {/* MyText.js에서 mystyle.module.css import 하기 */}
+        <h1 className={style.error}>Lorem ipsum dolor sit amet.</h1>
       </div>
     </>
   );
