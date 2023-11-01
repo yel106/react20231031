d993b37c0ea4500b10f51f24371bd568d10b0fa7
yel106 <yel_@hanmail.net>
2023-11-01 15:07:37 +0900

    default export, import 연습 완료 최종

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
       <MyBox />
       <h1>{person.name}</h1>
       <h1>{person.address}</h1>
-      <h1>{person.title}</h1>
-      <h1>{person.score}</h1>
+      <h1>{team.title}</h1>
+      <h1>{team.score}</h1>
     </div>
   );
 }
