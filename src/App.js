033dfd1a51efca4b47056c1d2a21efedec1f5a1d
yel106 <yel_@hanmail.net>
2023-11-01 10:15:53 +0900

    props 활용연습

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
 function App() {
   return (
     <div>
-
+      <MyText
+        value="연습하는 중"
+        style={{ color: "orange", backgroundColor: "yellow" }}
+      />
+      <MyText
+        value="연습하는 중"
+        style={{ color: "pink", backgroundColor: "black" }}
+      />
     </div>
   );
 }
 
+function MyText(props) {
+  return (
+    <div style={props.style}>
+      <h1>{props.value}</h1>
+    </div>
+  );
+}
 export default App;
