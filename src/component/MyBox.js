891130338d0e363e7d611772058f9a272b1f210c
yel106 <yel_@hanmail.net>
2023-11-01 15:27:43 +0900

    보통의 css 파일 사용하기

--- src/component/MyBox.js
+++ src/component/MyBox.js
@@ -1,9 +1,9 @@
-export default function MyBox() {
+import "../style/style1.css";
+
+export function MyBox() {
   return (
-    <div>
-      <h1>THIS IS MY BOX</h1>
-    </div>
+    <>
+      <h1 className="boxClass">Lorem ipsum dolor sit amet.</h1>
+    </>
   );
 }
-
-export const address = "신촌";
