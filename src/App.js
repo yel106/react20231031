f949c397e595ad13cd8a3c607a7ba697a3e5791e
yel106 <yel_@hanmail.net>
2023-11-01 11:34:03 +0900

    구조 분해 할당, 기본값 주기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
 function App() {
   return (
     <div>
-
+      <MyComp m="30px" p="10px" color="gold" bg="black" text="hello react" />
+      <MyComp m="30px" p="10px" color="gold" bg="black" />
+      <MyComp m="30px" bg="black" text="hello react" />
+      <MyComp p="10px" color="black" text="hello react" />
     </div>
   );
 }
 
+function MyComp({
+  m = "20px",
+  p = "10px",
+  color = "pink",
+  bg = "yellow",
+  text = "헬로우 리액트",
+}) {
+  return (
+    <div style={{ margin: m, padding: p, color: color, backgroundColor: bg }}>
+      <p3>{text}</p3>
+    </div>
+  );
+}
 export default App;
