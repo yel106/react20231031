b727a9ab8b34ec8d3d35c4c3696b62fda93f6bca
yel106 <yel_@hanmail.net>
2023-11-01 11:14:54 +0900

    props와 dstructuring assignment(구조 분해 할당) 연습3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
 function App() {
   return (
     <div>
-
+      <MyBox text="곧 점심시간이다" p={50} m={10} color="black" bg="skyblue" />
     </div>
   );
 }
 
+function MyBox({ p, m, bg, color, text }) {
+  return (
+    <div style={{ padding: p, margin: m, color: color, backgroundColor: bg }}>
+      <h1>{text}</h1>
+    </div>
+  );
+}
 export default App;
