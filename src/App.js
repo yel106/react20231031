f001fb7b1ae867a756d08013bfc569ac47d0e1a1
yel106 <yel_@hanmail.net>
2023-11-01 10:31:48 +0900

    props와 dstructuring assignment(구조 분해 할당)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 function App() {
   return (
-    <div>
-      <MyText
-        value="연습하는 중"
-        style={{ color: "orange", backgroundColor: "yellow" }}
-      />
-      <MyText
-        value="연습하는 중"
-        style={{ color: "pink", backgroundColor: "black" }}
-      />
-    </div>
+    <>
+      <MyComp value="흥민" address="신촌" />
+      <MyComp value="강인" address="강남" />
+    </>
   );
 }
 
-function MyText(props) {
+// {value: "흥민", address: "신촌"}
+function MyComp({ value, address }) {
+  // let {value, address} = props;
+  //구조 분해 할당은 자바스크립트의 argument에 적용
+  //  props 쓰는 것보다 구조 분해 할당을 더 자주 씀
   return (
-    <div style={props.style}>
-      <h1>{props.value}</h1>
+    <div>
+      <h1>
+        {value}이는 {address}에 산다.
+      </h1>
     </div>
   );
 }
