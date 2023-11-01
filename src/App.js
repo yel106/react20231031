26b1ad7e802c7c995a08757228b788b33c1630aa
yel106 <yel_@hanmail.net>
2023-11-01 12:03:09 +0900

    props와 distucturing assignment(구조분해할당) 연습3 최종

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 function App() {
   return (
     <>
-    <MyComp name="흥민" address="신촌" age={33} city="서울" />
-    <MyComp address="강남 age={22} city="부산" />
-    <MyComp address="런던" city="영국" />
-    <MyComp address="희찬" age={66} city="파리" address="프랑스""/>
-  </>
+      <MyComp name="흥민" address="신촌" age={33} city="서울" />
+      <MyComp address="런던" city="영국" />
+      <MyComp address="신촌" age={33} city="서울" />
+      <MyComp name="희찬" address="프랑스" age={66} city="파리" />
+    </>
   );
 }
 
-
-function MyComp({name="anonymous", address, city, age=100}) {
+function MyComp({
+  name = "익명",
+  address = "미국",
+  city = "애틀랜타",
+  age = 100,
+}) {
   return (
     <div>
       <p>
         {name}은 {age}세 이고 {address}, {city}에 산다
       </p>
     </div>
-  )
+  );
 }
 
 export default App;
