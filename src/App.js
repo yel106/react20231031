19acc43a96c304ec7d4c3d33d6c577d56e4c0b88
yel106 <yel_@hanmail.net>
2023-11-01 11:22:56 +0900

    props와 dstructuring assignment(구조 분해 할당) 연습3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 function App() {
   return (
-    <div>
-      <MyBox text="곧 점심시간이다" p={50} m={10} color="black" bg="skyblue" />
-    </div>
+    <>
+    <MyComp name="흥민" address="신촌" age={33} city="서울" />
+    <MyComp address="강남 age={22} city="부산" />
+    <MyComp address="런던" city="영국" />
+    <MyComp address="희찬" age={66} city="파리" address="프랑스""/>
+  </>
   );
 }
 
-function MyBox({ p, m, bg, color, text }) {
+
+function MyComp({name="anonymous", address, city, age=100}) {
   return (
-    <div style={{ padding: p, margin: m, color: color, backgroundColor: bg }}>
-      <h1>{text}</h1>
+    <div>
+      <p>
+        {name}은 {age}세 이고 {address}, {city}에 산다
+      </p>
     </div>
-  );
+  )
 }
+
 export default App;
