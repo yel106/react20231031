0c431429af25f09242c26cb68c0bcc373bd79f31
yel106 <yel_@hanmail.net>
2023-11-01 14:27:33 +0900

    default, import, export

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-
+import MyBox, { person, team } from "./component/MyContainer";
 function App() {
   return (
     <div>
-
+      <MyBox />
+      <h1>{person.name}</h1>
+      <h1>{person.address}</h1>
+      <h1>{person.title}</h1>
+      <h1>{person.score}</h1>
     </div>
   );
 }
---
+++ src/component/MyBox.js
@@ -0,0 +1,14 @@
+function MyBox() {
+  return (
+    <>
+      <div>
+        <h1>MY BOX</h1>
+      </div>
+    </>
+  );
+}
+
+export const name = "흥민";
+const address = "신촌";
+const title = "모름";
+const score = 44;
