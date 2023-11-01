aa6ae201549eafd172434d5ef8b6b299a32b9433
yel106 <yel_@hanmail.net>
2023-11-01 09:46:56 +0900

    props 연습1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
 function App() {
   return (
     <div>
-
+      <MyHeader color="white" bg="black" text="내가 만든 제목" />
+      <MyHeader color="orange" bg="skyblue" text="다시 만든 제목" />
+      {/*
+        <div style="color: white; background-color: black">
+          <h1> 내가 만든 제목 </h1>
+        </div>
+        <div style="color: orange; background-color: skyblue">
+          <h1> 다시 만든 제목 </h1>
+        </div>
+        */}
     </div>
   );
 }
 
+function MyHeader(props) {
+  return (
+    <>
+      <div style={{ color: props.color, backgroundColor: props.bg }}>
+        <h1> {props.text} </h1>
+      </div>
+    </>
+  );
+}
 export default App;
