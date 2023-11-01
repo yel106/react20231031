5fffa38320a91635dd9122af31f344230c3521ed
yel106 <yel_@hanmail.net>
2023-11-01 11:57:45 +0900

    component를 다른 파일에 작성 후 import, export 하기 연습완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { MyText } from "./component/MyText";
+import { MyBox } from "./component/MyBox";
+import { MyContainer } from "./component/MyContainer";
+import { MyNavbar } from "./component/MyNavbar";
 
 function App() {
   return (
     <div>
-
+      <MyText />
+      <MyBox />
+      <MyContainer />
+      <MyNavbar />
     </div>
   );
 }
