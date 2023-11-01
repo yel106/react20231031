f8af66b492b17d480ca82eb22732051f900894f5
yel106 <yel_@hanmail.net>
2023-11-01 10:45:58 +0900

    props와 dstructuring assignment(구조 분해 할당) 연습2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
 function App() {
   return (
     <div>
-
+      <MyElem name="흥민" age={44} team="토트넘" style={{ color: "orange" }} />
+      <MyElem name="강인" age={33} team="파리" style={{ color: "red" }} />
     </div>
+  );
+}
+
+function MyElem({ name, age, team, style }) {
+  return (
+    <>
+      <h1 style={style}>
+        {name}이는 {age}세 이고, {team} 소속이다.
+      </h1>
+    </>
   );
 }
 
