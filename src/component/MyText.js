f7309000e2a95f3cdb8827f2e2e0606ca58c7d93
yel106 <yel_@hanmail.net>
2023-11-01 16:40:39 +0900

    css module을 사용할 때 여러 클래스명을 주는 방법 연습-> teplate literal 또는 array.join으로 해결

--- src/component/MyText.js
+++ src/component/MyText.js
@@ -7,7 +7,12 @@ export function MyText() {
         {/* red 배경색, white 글자색 */}
         {/* src/style/mystyle.module.css 파일 생성 */}
         {/* MyText.js에서 mystyle.module.css import 하기 */}
-        <h1 className={style.error}>Lorem ipsum dolor sit amet.</h1>
+        <h1 className={`${style.error} ${style.special} ${style.coffee}`}>
+          Lorem ipsum dolor sit amet.
+        </h1>
+        <h1 className={[style.error, style.special, style.coffee].join(" ")}>
+          Lorem ipsum dolor sit amet.
+        </h1>
       </div>
     </>
   );
