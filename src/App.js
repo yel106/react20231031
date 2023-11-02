8850c2b966d78d501dcad16d3b1e035735bc1281
yel106 <yel_@hanmail.net>
2023-11-02 11:33:10 +0900

    simple grid 사용, 반응형 웹 적용

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Flex
-        w={"100%"}
-        h={"70px"}
-        bg={"gray.300"}
-        gap={3}
-        align={"center"}
-        justify={"space-around"}
-      >
-        {/* 1정도의 간격이 생김*/}
-        {/*Box*5>lorem1*/}
-        <Box bg={"gold"}>Lorem.</Box>
-        <Box bg={"red"}>Error!</Box>
-        <Box bg={"pink"}>Ut.</Box>
-        <Box bg={"teal"}>Dicta.</Box>
-        <Box bg={"yellow"}>Vel?</Box>
-      </Flex>
+      <SimpleGrid spacing={"10px"} columns={{ base: 2, md: 3, "2xl": 6 }}>
+        {" "}
+        {/*작을때 2개컬럼으로 보이고, 커지면 3개로 보이게*/}
+        {/*
+         base: "0em", // 0px
+          sm: "30em", // ~480px. em is a relative unit and is dependant on the font size.
+  md: "48em", // ~768px
+  lg: "62em", // ~992px
+  xl: "80em", // ~1280px
+        {/*Box[bg="blue.300"]*30>lorem1*/}
+        <Box bg="blue.300">Lorem.</Box>
+        <Box bg="blue.300">Iusto.</Box>
+        <Box bg="blue.300">Possimus!</Box>
+        <Box bg="blue.300">Beatae.</Box>
+        <Box bg="blue.800">Earum.</Box>
+        <Box bg="blue.300">Saepe?</Box>
+        <Box bg="blue.300">Provident.</Box>
+        <Box bg="blue.300">Harum!</Box>
+        <Box bg="blue.300">Recusandae.</Box>
+        <Box bg="blue.300">Harum.</Box>
+        <Box bg="blue.300">Animi!</Box>
+        <Box bg="blue.300">Reiciendis?</Box>
+        <Box bg="blue.300">Quisquam?</Box>
+        <Box bg="blue.300">Eius!</Box>
+        <Box bg="blue.300">Esse.</Box>
+        <Box bg="blue.300">Saepe!</Box>
+        <Box bg="blue.300">Ducimus.</Box>
+        <Box bg="blue.300">Rerum!</Box>
+        <Box bg="blue.300">Officia.</Box>
+        <Box bg="blue.300">Eaque!</Box>
+        <Box bg="blue.300">Tenetur.</Box>
+        <Box bg="blue.300">Ipsum.</Box>
+        <Box bg="blue.300">Eaque.</Box>
+        <Box bg="blue.300">A.</Box>
+        <Box bg="blue.300">Voluptatum!</Box>
+        <Box bg="blue.300">Temporibus.</Box>
+        <Box bg="blue.300">Culpa.</Box>
+        <Box bg="blue.300">Odio?</Box>
+        <Box bg="blue.300">Reprehenderit.</Box>
+        <Box bg="blue.300">Delectus!</Box>
+      </SimpleGrid>
     </>
   );
 }
