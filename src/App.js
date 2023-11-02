4dfa2fa574153e25951a4f052611f8db9f5e15ef
yel106 <yel_@hanmail.net>
2023-11-02 14:11:44 +0900

    Modal 사용 (팝업창 같은것)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import { Box, Center, Circle } from "@chakra-ui/react";
-import { MoonIcon, PhoneIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure(); /*구조분해 할당한 것 */
   return (
     <>
-      <Center bg={"gray.300"} h={"300px"}>
-        <Box bg={"white"} w={"100px"} h={"100px"}>
-          lorem.
-        </Box>
-      </Center>
+      <Button onClick={onOpen}>모달 열기</Button>
 
-      <Center bg={"pink.200"} h={"200px"}>
-        <Circle bg={"yellow.200"} w={"100px"} h={"100px"}>
-          <MoonIcon />
-        </Circle>
-      </Center>
+      <Modal
+        closeOnOverlayClick={false}
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        motionPreset="slideInTop"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>모달의 제목</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+            blanditiis cum cumque deleniti, dolore ducimus, esse exercitationem
+            impedit in iusto neque nisi nulla placeat quis quisquam rem
+            voluptate voluptates. Numquam!
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" onClick={onClose}>
+              닫기
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
