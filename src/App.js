58de4fbcd18e9fe6481300828caadc5eeace136c
yel106 <yel_@hanmail.net>
2023-11-02 15:25:54 +0900

    Switch( ex)알림 활성화 버튼) 사용

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { Box, FormControl, FormLabel, Textarea } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  FormControl,
+  FormLabel,
+  Switch,
+  Textarea,
+} from "@chakra-ui/react";
 
 function App(props) {
   return (
     <div>
       <Box w={"480px"}>
-        <FormControl>
-          <FormLabel>본문</FormLabel>
-          <Textarea
-            placeholder="본문을 작성하세요"
-            h={"700px"}
-            width={"1000px"}
-          ></Textarea>
+        <FormControl display={"flex"} alignItems={"center"}>
+          <FormLabel mb={0}>알림을 받으시겠습니까?</FormLabel>
+          <Switch size={"lg"} colorScheme="pink" />
         </FormControl>
       </Box>
     </div>
