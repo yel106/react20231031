b00d2342f2f7d9ea212312616873932f9bf79d9f
yel106 <yel_@hanmail.net>
2023-11-02 15:20:39 +0900

    Textarea(  ex) 본문 작성) 사용

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { Box, FormControl, Select } from "@chakra-ui/react";
+import { Box, FormControl, FormLabel, Textarea } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <div>
       <Box w={"480px"}>
         <FormControl>
-          <Select placeholder="선택하세요." multiple h={"120px"}>
-            {/*option[value=value$]*5>lorem1*/}
-            <option value="value1">Lorem.</option>
-            <option value="value2">Nostrum.</option>
-            <option value="value3">Voluptatum.</option>
-            <option value="value4">Dignissimos?</option>
-            <option value="value5">Consequuntur.</option>
-          </Select>
+          <FormLabel>본문</FormLabel>
+          <Textarea
+            placeholder="본문을 작성하세요"
+            h={"700px"}
+            width={"1000px"}
+          ></Textarea>
         </FormControl>
       </Box>
     </div>
