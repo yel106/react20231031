14f00a65cbdeb6347c0ed9e4efda25a8b5596db9
yel106 <yel_@hanmail.net>
2023-11-02 15:36:40 +0900

    Image 사용

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from "react";
-import {
-  Box,
-  Flex,
-  FormControl,
-  FormLabel,
-  Switch,
-  Textarea,
-} from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <div>
       <Box w={"480px"}>
-        <FormControl display={"flex"} alignItems={"center"}>
-          <FormLabel mb={0}>알림을 받으시겠습니까?</FormLabel>
-          <Switch size={"lg"} colorScheme="pink" />
-        </FormControl>
+        <Image src="https://bit.ly/dan-abramov" />
+      </Box>
+      <Box>
+        <Image boxSize={"200px"} src="https://bit.ly/dan-abramov" />
+      </Box>
+      <Box>
+        <Image boxSize={"100px"} src="https://bit.ly/dan-abramov" />
+      </Box>
+      <Box>
+        <Image
+          borderRadius={"full"}
+          boxSize={"200px"}
+          src="https://bit.ly/dan-abramov"
+        />
+      </Box>
+      <Box>
+        <Image src="adf.jpg" fallbackSrc="https://via.placeholder.com/150" />
+        {/* 이미지가 없을때 fallback이미지가 뜨도록 함 */}
       </Box>
     </div>
   );
