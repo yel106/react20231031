6af3e172809b5249313101db20d3a5d0c0addce2
yel106 <yel_@hanmail.net>
2023-11-02 10:19:01 +0900

    Box 컴포넌트, color 프로퍼티 사용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,32 @@
+import { Box } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Box bgColor="tomato">Lorem ipsum dolor.</Box>
+      <Box bgColor="red" color="white">
+        Lorem ipsum dolor.
+      </Box>
+      <Box bgColor="pink">Amet, aperiam, numquam.</Box>
+      <Box bgColor="blue.50">Deserunt, nihil optio.</Box>
+      <Box bgColor="blue.100">Dolor, fugiat nihil!</Box>
+      <Box bgColor="blue.900" color="white">
+        Numquam, optio reprehenderit.
+      </Box>
+      <Box bgColor="gray.500" color="facebook.400">
+        Eaque, molestiae, veniam.
+      </Box>
+      <Box bgColor="gray.100">Blanditiis ea, reiciendis!</Box>
+      <Box bgColor="orange.300" color="pink.500">
+        Ipsam maxime, minus.
+      </Box>
+      <Box bgColor="yellow" color="red">
+        Aperiam minima, natus.
+      </Box>
+      <Box bgColor="purple" color="white">
+        Nisi, non quos.
+      </Box>
+    </>
+  );
 }
 export default App;
