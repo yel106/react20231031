52252e3b594a7e353034d260f49a04a9fbb6f031
yel106 <yel_@hanmail.net>
2023-11-02 09:34:26 +0900

    필요없는 파일들 삭제함

--- src/App.js
+++ src/App.js
@@ -1,15 +1,4 @@
-import { MyBox } from "./component/MyBox";
-import { MyComp } from "./component/MyComp";
-import { MyContainer } from "./component/MyContainer";
-
-// chakraui css libarary >주로 이걸사용
 function App() {
-  return (
-    <>
-      <MyComp />
-      <MyBox />
-      <MyContainer />
-    </>
-  );
+  return <></>;
 }
 export default App;
