5ed8a014e2be6ad2135188f038be0a0a4337040c
yel106 <yel_@hanmail.net>
2023-11-02 14:34:20 +0900

    Button 사용

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
-import { Button, useToast } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { ArrowRightIcon, EmailIcon } from "@chakra-ui/icons";
 
 function App() {
-  const toast = useToast();
-
   return (
     <>
-      <Button
-        onClick={() =>
-          toast({
-            title: "저장 완료",
-            description: "게시물이 저장되었습니다.",
-            duration: 10000,
-            position: "top-right",
-            isClosable: true,
-          })
-        }
-      >
-        {" "}
-        성공 토스트 띄우기{" "}
+      <Button colorScheme="teal">lorem.</Button>
+      <Button colorScheme="facebook">Lorem.</Button>
+      <Button colorScheme="blue">Aliquid?</Button>
+      <Button colorScheme="orange">Sit.</Button>
+      <Button colorScheme="pink">Esse!</Button>
+      <Button colorScheme="red" size={"sm"}>
+        Quis!
       </Button>
-      <Button
-        onClick={() =>
-          toast({
-            title: "저장 실패!",
-            description: "게시물이 저장되지 않았습니다.",
-            duration: 10000,
-            position: "top-right",
-            isClosable: true,
-            status: "error",
-          })
-        }
-      >
-        {" "}
-        실패 토스트 띄우기{" "}
+      <Button colorScheme="cyan" size={"lg"} variant={"outline"}>
+        Deleniti.
       </Button>
+      <Button colorScheme="cyan" variant="ghost">
+        Laborum.
+      </Button>
+      <Button variant={"link"}>Consequuntur.</Button>
+      <Button leftIcon={<EmailIcon />}>Et?</Button>
+      <Button rightIcon={<ArrowRightIcon />}>Ipsam?</Button>
+      <Button isLoading loadingText={"전송 중"}>
+        A.
+      </Button>{" "}
+      {/*로딩 중일 땐 클릭이 안되게*/}
+      <Button isLoading>Commodi!</Button>
+      <Button>Repellat!</Button>
+      <Button>Quidem.</Button>
+      <Button>Incidunt.</Button>
+      <Button>Distinctio!</Button>
+      <Button>Aperiam.</Button>
+      <Button>Nesciunt!</Button>
+      <Button>Cumque.</Button>
+      <Button>Tempora.</Button>
+      <Button>Tenetur?</Button>
+      <Button>Vel!</Button>
+      <Button>Cumque.</Button>
+      <Button>Ducimus.</Button>
+      <Button>Mollitia?</Button>
+      <Button>Placeat?</Button>
+      <Button>Consectetur?</Button>
+      <Button>Magnam!</Button>
+      <Button>Necessitatibus.</Button>
+      <Button>Accusamus?</Button>
     </>
   );
 }
