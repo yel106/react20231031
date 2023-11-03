1500ba1d1d97daae477afc5aee63d57225ccd876
yel106 <yel_@hanmail.net>
2023-11-03 11:45:03 +0900

    event handler method에 event객체가 전달됨. Input과 onChange

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from "react";
-import { Button } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 
 function App(props) {
-  //브라우저는 이벤트 핸들러 메소드에
-  // event 객체를 매개값으로 넣어줌
-
-  function handleClick(event) {
-    // console.log(event); /* 버튼 누르면 객체의 내용이 출력됨*/
-    console.log(event.target); /* 실제 이벤트가 일어난 element의 모습이 나옴.*/
-    // console.log(event.target.className); /*실제 버튼의 클래스 네임이 나옴*/
-    // console.log(event.target.type);
+  function handleChange(e) {
+    // console.log("인풋값이 변경됨");
+    console.log(e.target.value);
   }
 
   return (
     <div>
-      <Button colorScheme="orange" onClick={handleClick}>
-        button1
-      </Button>
-      <Button colorScheme="facebook" onClick={handleClick}>
-        button2
-      </Button>
+      <Input onChange={handleChange} />
+      <Input onChange={handleChange} />
     </div>
   );
 }
