6453fce4a90113b0c07f45f9998ee997667ef27f
yel106 <yel_@hanmail.net>
2023-11-03 14:56:59 +0900

    state가 바뀌면 component가 다시 그려짐(re-render) 상태를 변경할 때 set메소드 사용해야함

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import React, { useState } from "react";
-import { Button, Input } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
 
 function App(props) {
-  // 상태(state)가 변경(이전 state와 달라짐) 되면
-  // component를 다시 그림 (re render)
-
-  const [message, setMessage] = useState(""); //배열을 리턴. 배열에 넣으면 구조분해 할당으로 state만 써도 됨
-  // parameter에 0을 넣으면 초기값이 0이 됨. ""을 넣으면 뭘 쓰냐에 따라 값이 달라짐
-  // index 0: 현재상태(state)
-  // index 1: state를 변경할 수 있는 메소드. (변경하고 안하고는 리액트가 관리함)
-
-  let text = "초기 메시지";
-  function handleInputChange(e) {
-    text = e.target.value;
-    console.log("text", text);
-  }
-
-  function handleButtonClick(e) {
-    setMessage("바뀐 상태");
+  // let number = 0; 이렇게 하면 리렌더링 안됨
+  let [number, setNumber] = useState(0); //상태 변경했지만 let으로 했을땐 버튼 눌러도 변하지 않음
+  //상태를 리액트가 관리하기 떄문에 리액트가 제공하는 메소드를 이용해서 변경해야함
+  function handleClick() {
+    //상태를 리액트가 관리하기 떄문에 리액트가 제공하는 메소드를 이용해서 변경해야함
+    // number++;
+    setNumber(number + 1); //setNumber를 이용해야 변경됨. number++라고 쓰면 후위연산이라 두번 눌러야 바뀜. number+1로 써야함
   }
   return (
     <div>
-      <Input onChange={handleInputChange} />
-      <p>{text}</p>
-      <Button onClick={handleButtonClick}>상태 바꾸기</Button>
-      <Button onClick={() => setMessage("")}> 빈 문자열로 바꾸기</Button>
-      <p>{message}</p>
+      <Button onClick={handleClick}>바꾸기</Button>
+      <Text>{number}</Text>
     </div>
   );
 }
