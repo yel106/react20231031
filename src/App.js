c6e9cd741f8237e0b37345cb1c7b4737a48eb459
yel106 <yel_@hanmail.net>
2023-11-03 12:33:27 +0900

    event.preventDefault() :기본 기능 취소.  연습 완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import { Box } from "@chakra-ui/react";
 
 function App() {
-  return <></>;
+  function handleClick(e) {
+    // a 태그 클릭시 naver로 이동하도록 하기
+    e.preventDefault();
+    window.location.href = "https://www.naver.com";
+  }
+
+  return (
+    <>
+      <a href="https://www.daum.net" onClick={handleClick}>
+        다음으로 이동
+      </a>
+    </>
+  );
 }
 export default App;
