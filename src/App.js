9f5f1ea21e6001039e0121afc1f769adf8f27d4e
yel106 <yel_@hanmail.net>
2023-11-03 14:01:53 +0900

    event.preventDefault() :기본 기능 취소.  연습 완료

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   function handleClick(e) {
     // a 태그 클릭시 naver로 이동하도록 하기
     e.preventDefault();
+
+    /*  location: js에서 URL 정보를 가져오는 객체임 */
+    /* lacation.href, location.protocol, location.host 등이 있음
+     * location.href는 프로퍼티 값이 변경되는 경우 새로운 페이지로 이동함.*/
     window.location.href = "https://www.naver.com";
   }
 
