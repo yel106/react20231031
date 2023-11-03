0cbf4a09d484302fbfac94169fb1058d254b99b5
yel106 <yel_@hanmail.net>
2023-11-03 09:33:44 +0900

    children prop 연습완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
+import * as PropTypes from "prop-types";
+
+function MyLink({ to, children }) {
+  return (
+    <>
+      <a href={to}> {children} </a>
+      {/* 버튼으로 만든느 방법 */}
+      <Button as="a" href={to} colorScheme="blue">
+        {children}
+      </Button>
+    </>
+  );
+}
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <MyLink to="https://www.naver.com">naver 로</MyLink>
+      <MyLink to="https://www.daum.net">daum으로</MyLink>
+      {/*
+    <a href="https://www.naver.com">naver로 </a>
+    <a href="https://www.daum.net">daum으로 </a>
+    */}
+    </>
+  );
 }
+
 export default App;
