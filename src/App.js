b7262711793fe3801a732f6751d3199d4b754bae
yel106 <yel_@hanmail.net>
2023-11-03 10:48:42 +0900

    arrow function을 prop으로 전달. 연습

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 
-function App() {
-  return <></>;
+function App({}) {
+  return (
+    <>
+      {/* 첫 번째 버튼 클릭시 "first" 콘솔에 출력 */}
+      <Button onClick={() => console.log("first")}>Lorem.</Button>
+
+      {/* 두번째 버튼 클릭시 "second" 콘솔에 출력 */}
+      <Button onClick={() => console.log("second")}>Quas.</Button>
+    </>
+  );
 }
+
 export default App;
