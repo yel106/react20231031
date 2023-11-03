f1f17f6f7ae0bbc66c05ee1485b391a561330104
yel106 <yel_@hanmail.net>
2023-11-03 11:40:32 +0900

    event handler method에 event객체가 전달됨

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import React from "react";
 import { Button } from "@chakra-ui/react";
 
 function App(props) {
-  function handleClickButton1() {
-    console.log("1번 버튼 클릭됨");
-  }
+  //브라우저는 이벤트 핸들러 메소드에
+  // event 객체를 매개값으로 넣어줌
 
-  function handleClickButton2() {
-    console.log("2번 버튼 클릭됨");
-  }
-
-  function handleButtonClick(number) {
-    console.log(number + "번째 버튼 클릭됨");
+  function handleClick(event) {
+    // console.log(event); /* 버튼 누르면 객체의 내용이 출력됨*/
+    console.log(event.target); /* 실제 이벤트가 일어난 element의 모습이 나옴.*/
+    // console.log(event.target.className); /*실제 버튼의 클래스 네임이 나옴*/
+    // console.log(event.target.type);
   }
 
   return (
     <div>
-      <Button onClick={handleClickButton1}>첫번째 버튼</Button>
-      <Button onClick={handleClickButton2}>두번째 버튼</Button>
-      <Button onClick={() => handleButtonClick(3)}>세번째 버튼</Button>
-      <Button onClick={() => handleButtonClick(4)}>네번째 버튼</Button>
+      <Button colorScheme="orange" onClick={handleClick}>
+        button1
+      </Button>
+      <Button colorScheme="facebook" onClick={handleClick}>
+        button2
+      </Button>
     </div>
   );
 }
