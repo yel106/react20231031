7bfe65cd0f4e7f56c9d7a16ff19ee18793155bf4
yel106 <yel_@hanmail.net>
2023-11-03 15:46:55 +0900

    예제) state를 사용해서 dom 그리기 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import { Box } from "@chakra-ui/react";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  function handleInput1Change(e) {
+    setNumber1(Number(e.target.value));
+  }
+
+  function handleInput2Change(e) {
+    setNumber2(Number(e.target.value));
+  }
+
+  let result = number1 + number2;
+
+  return (
+    <>
+      <input type="number" onChange={handleInput1Change} /> +
+      <input type="number" onChange={handleInput2Change} />= {result}
+    </>
+  );
 }
 export default App;
