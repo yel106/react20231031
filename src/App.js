8ad0c7d1f4623a412be640fdeb5856e8e427e8ee
yel106 <yel_@hanmail.net>
2023-11-06 12:23:14 +0900

    배열을 jsx에서 사용하기, ListItems에 매핑되는 컴포넌트는 key prop을 꼭 가져야 함
    key prop은 유일한 값이어야 하며, 주로 db의 primary key가 사용될 것이다.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from "react";
 import { Box, List, ListItem } from "@chakra-ui/react";
-
 function App(props) {
   const arr = ["pizza", "돈까스", "햄버거", "김치찌개"];
   const arr2 = ["손흥민", "이강인", "김민재"];
   const arr3 = ["라떼", "에스프레소", "카푸치노"];
+  const arr4 = [
+    { id: 0, name: "라떼" },
+    { id: 1, name: "에스프레소" },
+    { id: 2, name: "카푸치노" },
+  ];
 
-  const listItems = arr.map((item) => <ListItem>{item}</ListItem>);
+  const listItems = arr.map((item, index) => (
+    <ListItem key={index}>{item}</ListItem>
+  ));
 
   return (
-    // <div>
-    //   <List>
-    //     <ListItem>{arr[0]}</ListItem>
-    //     <ListItem>{arr[1]}</ListItem>
-    //     <ListItem>{arr[2]}</ListItem>
-    //     <ListItem>{arr[3]}</ListItem>
-    //   </List>
-    // </div>
-
-    //위와 같은 결과가 나옴
-
     <div>
       <Box mb={3}>
         <List>{listItems}</List>
@@ -29,20 +24,39 @@ function App(props) {
         <List>
           {/*  손흥민, 이강인, 김민재
            */}
-          {arr2.map((e) => (
-            <ListItem>{e}</ListItem>
+          {arr2.map((e, index) => (
+            <ListItem key={index}>{e}</ListItem>
           ))}
         </List>
       </Box>
+
       <Box mb={3}>
         <List>
           {/*  세번째 배열들이 ListItem으로 출력되도록 */}
-          {arr3.map((e) => (
-            <ListItem>{e}</ListItem>
+          {arr3.map((e, i) => (
+            <ListItem key={i}>{e}</ListItem>
+          ))}
+        </List>
+      </Box>
+
+      <Box>
+        <List>
+          {arr3.map((e, j) => (
+            <ListItem key={j}>{e}</ListItem>
           ))}
         </List>
       </Box>
     </div>
+
+    //위와 같은 결과가 나옴
+    // <div>
+    //   <List>
+    //     <ListItem>{arr[0]}</ListItem>
+    //     <ListItem>{arr[1]}</ListItem>
+    //     <ListItem>{arr[2]}</ListItem>
+    //     <ListItem>{arr[3]}</ListItem>
+    //   </List>
+    // </div>
   );
 }
 export default App;
