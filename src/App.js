5c9ade5d73b4294a2a54ec2f1a72ea8ad54a7887
yel106 <yel_@hanmail.net>
2023-11-06 14:36:42 +0900

    state가 배열이면 새 state는 기존 배열을 복사해서 새 배열로 사용해야함

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
 import React, { useState } from "react";
-import { Input, List, ListItem, Button } from "@chakra-ui/react";
+import { Box, Button, ListItem, UnorderedList } from "@chakra-ui/react";
 
 function App(props) {
-  const [coffees, setCoffees] = useState(["라떼"]); //배열 객체 들어있음
-  const [text, setText] = useState("");
+  const [foods, setFoods] = useState([]);
 
-  function handleButtonClick() {
-    // 직전 상태 배열을 조작하면 안되고
-    // coffees.push(text);
-    // setCoffees(coffees); // 객체인 배열이 들어있어서 같은 객체라서 참조값이 같으니까 setCoffees해도 안 바뀜
-    // console.log(coffees);
+  function handleButtonClick(food) {
+    setFoods([...foods, food]);
+  }
 
-    // 새 배열을 만들어서 set state 해야 함
-    const newCoffees = [...coffees];
-    newCoffees.push(text);
-    setCoffees(newCoffees); // 객체인 배열이 들어있어서 같은 객체라서 참조값이 같으니까 setCoffees해도 안 바뀜
+  function handleRemoveButtonClick(index) {
+    // console.log(index + "번째 아이템 지우기");
+    // const nextFoods = [...foods];
+    // nextFoods.splice(index, 1); //index에 있는거 1개 지움
+
+    // const nextFoods = foods.filter((item, i) => i != index); //i랑 index랑 다른애만 살아남도록 필터함
+    // setFoods(nextFoods);
+
+    setFoods(foods.filter((item, i) => i != index));
   }
 
   return (
     <div>
-      <Input value={text} onChange={(e) => setText(e.target.value)} />
-      <Button onClick={handleButtonClick}>추가</Button>
-
-      <List>
-        {coffees.map((c, index) => (
-          <ListItem key={index}>{c}</ListItem>
-        ))}
-      </List>
+      {/*Button[onClick]{}*3*/}
+      <Button onClick={() => handleButtonClick("커피")}>커피</Button>
+      <Button onClick={() => handleButtonClick("케잌")}>케잌</Button>
+      <Button onClick={() => handleButtonClick("아이스커피")}>
+        아이스커피
+      </Button>
+      <Box>
+        <UnorderedList>
+          {foods.map((item, index) => (
+            <ListItem key={index}>
+              {item}{" "}
+              <Button onClick={() => handleRemoveButtonClick(index)}>
+                지우기
+              </Button>
+            </ListItem>
+          ))}
+        </UnorderedList>
+      </Box>
     </div>
   );
 }
