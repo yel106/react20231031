68fea063642f0093178dc49d911ff153ebdac6bf
yel106 <yel_@hanmail.net>
2023-11-06 14:18:37 +0900

    배열인 상태를 복사해서 상태변경. 연습완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
-import { Box } from "@chakra-ui/react";
+import { useState } from "react";
+import { Box, Button, ListItem, OrderedList } from "@chakra-ui/react";
 
 function App() {
-  return <></>;
+  const [items, setItems] = useState([]);
+
+  function handleButtonClick(item) {
+    // items의 상태를 item을 추가하도록 변경
+    // const nextItems = [...items];
+    // nextItems.push(item);
+
+    // const nextItems = [...items, item];
+    // setItems(nextItems);
+
+    //위에서 쓴 것과 동일한 결과가 나옴
+    setItems([...items, item]);
+  }
+
+  return (
+    <>
+      <Button onClick={() => handleButtonClick("커피")}>커피</Button>
+      <Button onClick={() => handleButtonClick("케잌")}>케잌</Button>
+      <Button onClick={() => handleButtonClick("아이스크림")}>
+        아이스크림
+      </Button>
+      <Button onClick={() => handleButtonClick("크로아상")}>크로아상</Button>
+      <Button onClick={() => handleButtonClick("단팥빵")}>단팥빵</Button>
+
+      <Box>
+        <OrderedList>
+          {items.map((item, index) => (
+            <ListItem key={index}>{item}</ListItem>
+          ))}
+        </OrderedList>
+      </Box>
+    </>
+  );
 }
+
 export default App;
