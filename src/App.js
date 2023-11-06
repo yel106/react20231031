267660a1382f97d01bd125ea4456d356b93b82ab
yel106 <yel_@hanmail.net>
2023-11-06 09:38:08 +0900

    state 사용 예제 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import { Box } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [number, setNumber] = useState(0);
+
+  function handleButtonClick() {
+    setNumber(number + 1);
+  }
+
+  function handleMinusButtonClick() {
+    setNumber(number - 1);
+  }
+  return (
+    <>
+      <h1>{number}</h1>
+      <Button onClick={handleButtonClick}>증가</Button>
+      <Button onClick={handleMinusButtonClick}>감소</Button>
+    </>
+  );
 }
 export default App;
