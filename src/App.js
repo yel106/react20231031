56023cf22c243fc109985cab02df220c2a417c54
yel106 <yel_@hanmail.net>
2023-11-07 11:15:34 +0900

    다른 컴포넌트의 상태 바꾸기: 공통 부모에게 상태를 두고 사용해야 함. 연습완료 -> arrow function으로 바꿔봄

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import * as PropTypes from "prop-types";
 import { Box, Input, Text } from "@chakra-ui/react";
 
 function MyInput({ address, onChange }) {
-  function handleInputChange(e) {
-    onChange(e.target.value);
-  }
+  const handleInputChange = (e) => onChange(e.target.value);
 
   return (
     <Box>
-      <Input value={address} onChange={handleInputChange} />
+      <Input value={address} onChange={(e) => onChange(e.target.value)} />
     </Box>
   );
 }
 
 function MyText({ address }) {
-
   return (
     <Box>
       <Text>{address}</Text>
@@ -26,13 +23,11 @@ function MyText({ address }) {
 function App(props) {
   const [address, setAddress] = useState("");
 
-  function handleInputChange(text) {
-    setAddress(text);
-  }
+  const handleInputChange = (text) => setAddress(text);
 
   return (
     <div>
-      <MyInput address={address} onChange={handleInputChange} />
+      <MyInput address={address} onChange={(text) => setAddress(text)} />
       <MyText address={address} />
     </div>
   );
