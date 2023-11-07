6e269c42687b0eb16e4b379932fdfc2ffa49e516
yel106 <yel_@hanmail.net>
2023-11-07 14:16:38 +0900

    context value에 {state, setState}를 넣기

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
 import React, { createContext, useContext, useState } from "react";
-import { Text, Button } from "@chakra-ui/react";
-function Ccomp() {
-  //3. context 사용하기 : useContext(Context);
-  const message = useContext(Messagecontext);
+import { Button, Text } from "@chakra-ui/react";
 
-  return <Text>받은 메시지: {message}</Text>;
+function AComp() {
+  // 3. context 사용하기 : useContext(Context)
+  const { setMessage } = useContext(MessageContext);
+
+  return (
+    <Button onClick={() => setMessage("변경된 메세지")}>
+      메시지 변경하기!!
+    </Button>
+  );
 }
 
-function Bcomp() {
-  return <Ccomp />;
-}
+function BComp() {
+  //3. context 사용하기: useContext(Context)
+  // const value = useContext(MessageContext);
 
-function Acomp() {
-  return <Bcomp />;
+  const { message } = useContext(MessageContext);
+
+  return <Text>{message}</Text>;
 }
 
 function App(props) {
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("초기 상태 메시지");
 
-  //message state를 Ccomp에 전달하기
-  // 1. context만들기 : createContext();로 만들기
-  // 2. context에 state 넣기 . 상위 컴포넌트를 Messagecontext로 감싸줌
-  //        :<Context.Provide value={state}></Context.Provider>
-  // 3. tree 안에 context 사용하기
+  const value = { message: message, setMessage: setMessage };
+
   return (
     <div>
-      <Button onClick={() => setMessage("바꾼 메시지!")}>메시지 바꾸기</Button>
-      <Messagecontext.Provider value={message}>
-        <Acomp />
-      </Messagecontext.Provider>
+      {/* 2. context에 value 넣기: context.Provider value={state}> 컴포넌트넣기</Context.Provider> */}
+      <MessageContext.Provider
+        value={{
+          message,
+          setMessage,
+        }}
+      >
+        <AComp />
+        <BComp />
+      </MessageContext.Provider>
     </div>
   );
 }
-// 1. Context 만들기
-// context 이름은 대문자로 시작하고 Context로 끝남
-const Messagecontext = createContext(null);
 
+// 1. context 만들기: createContext(null)
+const MessageContext = createContext(null);
 export default App;
