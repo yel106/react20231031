0e0ec2fb586e5ced89c04d696eb24b2a170f957d
yel106 <yel_@hanmail.net>
2023-11-08 15:32:41 +0900

    react-router사용시엔 a링크가 아닌 react router의 Link를 사용할 것

--- src/App.js
+++ src/App.js
@@ -2,15 +2,33 @@ import React from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  Link,
   Outlet,
   Route,
   RouterProvider,
 } from "react-router-dom";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 function HomeComponent() {
   return (
     <Box>
+      <Flex gap={"10px"}>
+        <Box>
+          {/* react-router 사용 시 a태그 사용하지 않을 것. 전체 페이지가 로딩됨*/}
+          <a href="/apath">에이로 가기</a>
+        </Box>
+        <Box>
+          <a href="/bpath">비로 가기</a>
+        </Box>
+
+        {/* 대신 Link Component 사용 */}
+        <Box>
+          <Link to={"/apath"}>에이로 가기</Link>
+        </Box>
+        <Box>
+          <Link to={"/bpath"}>비로 가기</Link>
+        </Box>
+      </Flex>
       공통된 요소들
       <Outlet />
     </Box>
