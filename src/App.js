9fe786b84af8085ce7b256aa46178ebe08fd2ab9
yel106 <yel_@hanmail.net>
2023-11-08 14:31:35 +0900

    component의 초기렌더링과 재 랜더링

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
 import React, { useEffect, useState } from "react";
+import { Box, Button, Checkbox, Text } from "@chakra-ui/react";
+
+function MyComp({ color }) {
+  const [number, setNumber] = useState(0);
+
+  useEffect(() => {
+    console.log(color + " : initial render");
+  }, []);
+  console.log(color + " : re render");
+
+  return (
+    <Box borderWidth={"10px"} borderColor={color}>
+      <Button onClick={() => setNumber(number + 1)}>증가</Button>
+      <Text>{number}</Text>
+    </Box>
+  );
+}
 
 function App(props) {
-  const [number, setNumber] = useState(0);
-  // useEffect 사용 시 자주하는 실수
-  useEffect(() => {
-    console.log("코드 실행");
-    // setNumber(number + 1); // trigger하는 값을 변경하면 안 됨
-  }, [number]);
+  //부모가 렌더링될 때 자식도 렌더링 됨
 
-  return <div></div>;
+  const [number, setNumber] = useState(0);
+  const [secondToggle, setSecondToggle] = useState(true);
+  const [goldToggle, setGoldToggle] = useState(true);
+
+  console.log("부모 re-render");
+  return (
+    <div>
+      <Box>
+        <Text>부모</Text>
+        <Button onClick={() => setNumber(number + 1)}>증가</Button>
+        <Text>{number}</Text>
+        <Checkbox
+          defaultChecked={true}
+          onChange={(e) => setSecondToggle(e.target.checked)}
+        ></Checkbox>{" "}
+        파란 박스 토글
+        <Checkbox
+          defaultChecked={true}
+          onChange={(e) => setGoldToggle(e.target.checked)}
+        ></Checkbox>{" "}
+        골드 박스 토글
+      </Box>
+
+      <Box mt={5}>
+        <Text>자식들</Text>
+        <MyComp color={"red"} />
+
+        {secondToggle && <MyComp color={"blue"} />}
+        {/*파란 박스 토글은 체크박스 클릭하면 새로운 상태로 시작됨*/}
+
+        <Box sx={{ display: goldToggle ? "block" : "none" }}>
+          {/* 토글 체크박스 클릭 했을때 다시 시작하지 않고 상태 유지하게 하려면 css로 보이거나 보이지 않게 하기 */}
+          <MyComp color={"gold"} />
+        </Box>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
